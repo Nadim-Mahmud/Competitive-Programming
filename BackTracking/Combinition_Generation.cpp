7b2e74409686c206dde964730c424510b87dea0d
Nadim-Mahmud <mahmudnadim16@gmail.com>
2018-12-07 23:27:06 +0600

    Backtracking : All possible Combinition

---
+++ BackTracking/Combinition_Generation.cpp
@@ -0,0 +1,23 @@
+/**
+*   This code will print all nCr
+*   While ara is elements of n
+*/
+
+int ara[20],sln[20],n,r;
+
+void combinition(int pos,int last){
+    int i;
+    if(pos>r){
+        for(i=1;i<=r;i++){
+            if(i==r) printf("%d\n",sln[i]);
+            else printf("%d ",sln[i]);
+        }
+        return;
+    }
+
+    for(i=last+1;i<=n;i++){
+        sln[pos] = ara[i];
+        combinition(pos+1,i);
+    }
+}
+
