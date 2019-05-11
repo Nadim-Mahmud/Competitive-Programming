9d7129a9daf5ebfa620258cc283fd2c6b8f2c5bf
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-05-11 21:42:33 +0600

    algo

--- MyTemplate.cpp
+++ MyTemplate.cpp
@@ -336,7 +336,34 @@ for(i=1;i<=it;i++){
 }
 
 
+///       Gaussian Elemination [n^3]
 
+void gauss(){
+    for(i=0;i<n;i++){
+        l = i;
+        for(j=i+1;j<n;j++){
+            if(abs(ara[j][i])>abs(ara[l][i])){
+                l = j;
+            }
+        }
+        if(i!=l){
+            for(j=0;j<=n;j++){
+                swap(ara[i][j], ara[l][j]);
+            }
+        }
+        for(j=0;j<n;j++){
+            if(j!=i){
+                tmp = ara[j][i]/ara[i][i];
+                for(k=i;k<=n;k++){
+                    ara[j][k] -= tmp*ara[i][k];
+                }
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        ara[i][n] /= ara[i][i]; ///final result
+    }
+}
 
              ///******    Graph    ******///
 
