5818c1ee31eb8349cd120b91c704b839a32e129b
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-05-12 20:51:36 +0600

    permutation generator

---
+++ Source/BackTracking/Permutation_generation.cpp
@@ -0,0 +1,21 @@
+/**
+*   This code will print all nPr
+*   While ara contains n elements
+*/
+
+int ara[200],mr[200],k;
+
+void permutation(int a,int n){
+    int i;
+    if(a==n+1){
+        for(i=1;i<=n;i++) printf("%c",ara[i]);
+        printf("\n");
+        return ;
+    }
+    for(i=1;i<=n;i++) if(!mr[i]){
+        mr[i] = 1;
+        ara[a] = i;
+        permutation(a+1,n);
+        mr[i] = 0;
+    }
+}
