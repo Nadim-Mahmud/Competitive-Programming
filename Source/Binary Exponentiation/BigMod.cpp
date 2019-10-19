b80ea6455cc4b50df58c85ac86048535aa3b99a4
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-10-20 03:13:49 +0600

    bigmod added

---
+++ Source/Binary Exponentiation/BigMod.cpp
@@ -0,0 +1,15 @@
+/**
+    Description : calculate (n^r)%MD
+*/
+
+#define MD 1000007
+
+ll bigmod(ll n, ll r){
+    if(r==0) return 1;
+    if(r==1) return n%MD;
+    if(r%2==1) return (bigmod(n,r-1)*n)%MD;
+    else{
+        ll x = bigmod(n,r/2);
+        return (x*x)%MD;
+    }
+}
