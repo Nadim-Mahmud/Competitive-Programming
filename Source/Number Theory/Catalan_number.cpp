8cf16c006eb71215e1c1168a5ff05efa03cdcd6f
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-10-20 01:56:54 +0600

    catalan number descriptions added

--- Source/Number Theory/Catalan_number.cpp
+++ Source/Number Theory/Catalan_number.cpp
@@ -1,6 +1,27 @@
 ///Time Complexity =~ onetime n for factorial
 /// and in each query [log(n)]
 
+/**
+    n’th catalan number : Cn = 1/(n+1)(2n n) = (2n n) - (2n n+1)
+                =(2n)!/((2n-n)!*n!*(n+1))
+                =(2n)!/((n)!*(n+1)!)
+
+    C(0) = C(1) = 1
+    C(4) = C0*C3 + C1*C2 + C2*C1 + C3*C0
+    C(n) =C(0)*(n-1) +C(1)*C(n-2) +.......... +C(i)*C(n-i-1)+........+C(n-1)*C(0)
+
+    * the number of ways a polygon with n+2 sides can be cut into triangles
+    * the number of ways to use n rectangles to tile a stairstep shape (1, 2, ..., n−1, n).
+    * Cn counts the number of expressions containing n pairs of parentheses which are correctly matched
+    * Cn is the number of different ways (n + 1) factors can be completely parenthesized  catalan(3) = 5 , ex :  ((ab)c)d,   (a(bc))d ,   (ab)(cd) ,   a((bc)d) ,   a(b(cd))
+    * Count the number of possible Binary Search Trees with n keys
+    * Total number of full binary tree : catalan(n) * factorial(n)
+    * the number of paths of length 2n through an n-by-n grid that do not rise above the main diagonal
+    * Cn is the number of ways to form a “mountain ranges” with n upstrokes and n down-strokes that all stay above the original line.The mountain range interpretation is that the mountains will never go below the horizontal.
+    * Cn is the number of ways that the vertices of a convex 2n-gon can be paired so that the line segments joining paired vertices do not intersect. This is precisely the condition that guarantees that the paired edges can be identified (sewn together) to form a closed surface of genus zero (a topological 2-sphere).
+
+*/
+
 #define ll long long
 #define MX 1000005
 #define MD 100000007
