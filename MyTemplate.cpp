44cf4fe01ef2136b3cf0d31bf1167fbfd8a640d9
Nadim Mahmud <mahmudnadim16@gmail.com>
2019-11-05 18:31:39 +0600

    Update MyTemplate.cpp
    sgemnet tree and RMQ updated

--- MyTemplate.cpp
+++ MyTemplate.cpp
@@ -93,6 +93,46 @@ int query(int l,int r){
     return mn;
 }
 
+/**
+    Name : Sparse table(RMQ)
+    Description : Find min/max
+    Time Complexity : Build O(nlogn) Query O(1)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+//0 Indexed
+#define MX 10000
+int spt[MX][22];
+int n,ar[MX]={ 7, 2, 3, 0, 5, 10, 3, 12, 18 };
+void buildST()
+{
+	for (int i = 0; i < n; i++) spt[i][0] = ar[i];
+
+	for (int j = 1; (1 << j) <= n; j++) {
+		for (int i = 0; (i + (1 << j) - 1) < n; i++) {
+            spt[i][j] = min(spt[i + (1 << (j - 1))][j - 1] , spt[i][j - 1]);
+		}
+	}
+}
+
+int query(int l, int r)
+{
+    if(l>r) return INT_MAX;
+	int j = (int)log2(r - l + 1);
+	///j = 31 - __builtin_clz(r - l+1);
+	return min (spt[l][j], spt[r - (1 << j) + 1][j]);
+}
+
+// Driver program
+int main()
+{
+
+    n = 9;
+	buildST();
+	cout << query(4, 7) << endl;
+	cout << query(7, 8) << endl;
+	return 0;
+}
 
 
 ///   *** BIT O(Log(n)) space O(n)
@@ -278,6 +318,108 @@ int main() {
   return 0;
 }
 
+
+/**
+ * Name : Maximum subsegment sum
+ * Description: Segment Tree with custom merge function. .
+ * Usage: construct O(N), query O(lg(N)), update O(lg(N))
+ * Source: https://github.com/dragonslayerx
+ */
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+#define MAX 50100
+#define INF -1000000000
+
+struct node {
+    int sum;
+    int maxs, prefix, suffix;
+    node(){
+        sum = prefix = suffix = 0;
+        maxs = INF;
+    }
+
+    node(int sum, int maxs, int prefix, int suffix) {
+        setNode(sum, maxs, prefix, suffix);
+    }
+
+    void setNode(int sum, int maxs, int prefix, int suffix){
+        this->sum =sum;
+        this->maxs=maxs;
+        this->prefix=prefix;
+        this->suffix=suffix;
+    }
+};
+
+int a[MAX];
+node st[4*MAX];
+
+node merge(node left, node right){
+    node t;
+    t.prefix = max(left.prefix, left.sum+right.prefix);
+    t.suffix = max(right.suffix, right.sum+left.suffix);
+    t.sum = left.sum+right.sum;
+    t.maxs = left.maxs;
+    t.maxs = max(t.maxs, right.maxs);
+    t.maxs = max(t.maxs, left.suffix+right.prefix);
+    return t;
+}
+
+node construct(int n, int ll, int rl){
+    if (ll == rl) {
+        st[n].setNode(a[ll], a[ll], a[ll], a[ll]);
+    } else {
+        node left = construct(2*n+1, ll, (ll+rl)/2);
+        node right = construct(2*n+2, (ll+rl)/2+1, rl);
+        st[n] = merge(left, right);
+    }
+    return st[n];
+}
+
+node query(int n, int ll, int rl, int x, int y){
+    int mid = (ll+rl)/2;
+    if (x==ll &&  y==rl) return st[n];
+    else if (y <= mid) return query(2*n+1, ll, mid, x, y);
+    else if (x > mid) return query(2*n+2, mid+1, rl, x, y);
+    else {
+        node left = query(2*n+1, ll, (ll+rl)/2, x, mid);
+        node right = query(2*n+2, (ll+rl)/2+1, rl, mid+1, y);
+        return merge(left, right);
+    }
+}
+
+node update(int n, int ll, int rl, int p, int val){
+    if (p < ll || p > rl) return st[n];
+    if (p == ll &&  p == rl) {
+        st[n].setNode(val, val, val, val);
+        return st[n];
+    } else {
+        int mid = (ll+rl)/2;
+        node left = update(2*n+1, ll, (ll+rl)/2, p, val);
+        node right = update(2*n+2, (ll+rl)/2+1, rl, p, val);
+        st[n] = merge(left, right);
+    }
+    return st[n];
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", a+i);
+    construct(0, 0, n-1);
+    int q;
+    scanf("%d", &q);
+    while (q--) {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        x--, y--;
+        printf("%d\n", query(0, 0, n-1, x, y).maxs);
+    }
+}
+© 2019 GitHub, Inc.
 
 /**'
     Name : Disjoint set union find
